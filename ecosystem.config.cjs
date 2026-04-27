1b9c9cafbe97479568bbad2545f17a467d64f879
samuelarbibe <samuel@arbibe.org>
2026-04-27 19:22:34 +0300

    Update pm2 config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,19 +1,19 @@
 module.exports = {
   apps: [
     {
-      name: 'sports-partner',
-      cwd: './server',
-      script: 'dist/index.js',
+      name: "sports-partner",
+      cwd: "./server",
+      script: "dist/index.js",
       instances: 1,
-      exec_mode: 'fork',
+      exec_mode: "fork",
       env: {
-        NODE_ENV: 'production',
-        PORT: 80,
+        NODE_ENV: "production",
+        PORT: 3001,
       },
-      node_args: '--enable-source-maps',
+      node_args: "--enable-source-maps",
       watch: false,
-      max_memory_restart: '300M',
-      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+      max_memory_restart: "300M",
+      log_date_format: "YYYY-MM-DD HH:mm:ss Z",
       merge_logs: true,
     },
   ],
